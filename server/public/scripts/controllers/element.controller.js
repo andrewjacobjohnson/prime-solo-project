d6b5beac483168e732af2d178da053de8f6acfa5
Andrew Johnson <ajj@byu.net>
2018-02-14 10:05:13 -0600

    Counts instances of a selection in input text

--- server/public/scripts/controllers/element.controller.js
+++ server/public/scripts/controllers/element.controller.js
@@ -64,4 +64,62 @@ myApp.controller('ElementController', ['$http', '$routeParams', function($http, 
         console.log('done', self.newElement);
     };
 
+    // Explodes selection on edit
+    self.explodeSelection = function(sourceText) {
+
+        // need to check if the selection is in the scope we're editing
+        let text = "";
+        if (window.getSelection) {
+            text = window.getSelection().toString();
+        } else if (document.selection && document.selection.type != "Control") {
+            text = document.selection.createRange().text;
+        }
+
+        // var string = 'hi';
+        // var regex = /[0-9a-zA-Z]/;
+        // console.log(sourceText)
+        // var count = (sourceText.match(new RegExp(text, 'gi')) || []);
+        // console.log(count);
+
+        let resultsArray = [];
+
+        let f = 00;
+        // while (i > 0 && f < 100) {
+        //     i = sourceText.indexOf(text, i) + 1;
+        //     resultsArray.push(i);
+        //     console.log(resultsArray);
+        //     f++
+        // }
+
+        // current position
+        let i = 0;
+        let stopSign = 1;
+        do {
+            i = sourceText.indexOf(text, i);
+            // only if the substring is in the string, push its index to results
+            if(i > -1) {
+                resultsArray.push(i);
+                i++
+            }
+        } while (i > -1); // if it wasn't in the results, stop the loop
+        console.log(resultsArray);
+        // while (i !== -1) {
+            
+        //     if (i > -1) {
+                
+        //     } else {
+        //         break;
+        //     }
+        // }
+
+        // console.log(i);
+
+        // for (let i = 0; i < sourceText.length - 1 && i > 0; i) {
+        //     console.log(i);
+        //     i = sourceText.indexOf(text, i);
+        //     resultsArray.push(i);
+        // }
+
+        return sourceText;
+    };
 }]);
